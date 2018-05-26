94a967384f1ef545cca963b5b64dec5049bdf31b
Zach Gilbert <zsgilber@gmail.com>
2018-05-26 16:38:21 -0700

    syntax changes

--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -16,16 +16,16 @@ class TestUserService(BaseTestCase):
         self.assertIn('success', data['status'])
 
     def test_add_user(self):
-    """Ensure a new user can be added to the database."""
+        """Ensure a new user can be added to the database."""
         with self.client:
             response = self.client.post(
-            '/users',
-            data=json.dumps({
+                '/users',
+                data=json.dumps({
                 'username': 'michael',
                 'email': 'michael@mherman.org'
             }),
             content_type='application/json',
-        )
+            )
         data = json.loads(response.data.decode())
         self.assertEqual(response.status_code, 201)
         self.assertIn('michael@mherman.org was added!', data['message'])
