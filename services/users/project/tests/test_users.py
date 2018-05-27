bb15075e06b51cd3a4557bf11a0eb82b2b131923
Zach Gilbert <zsgilber@gmail.com>
2018-05-26 17:56:00 -0700

    test for getting all users

--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -5,6 +5,11 @@ from project.tests.base import BaseTestCase
 from project import db
 from project.api.models import User
 
+def add_user(username, email):
+    user = User(username=username, email=email)
+    db.session.add(user)
+    db.session.commit()
+    return user
 
 class TestUserService(BaseTestCase):
     """Tests for the Users Service."""
@@ -86,9 +91,7 @@ class TestUserService(BaseTestCase):
 
     def test_single_user(self):
         """Ensure get single user behaves correctly."""
-        user = User(username='test', email='test@test.com')
-        db.session.add(user)
-        db.session.commit()
+        user = add_user('test', 'test@test.com')
         with self.client:
             response = self.client.get(f'/users/{user.id}')
             data = json.loads(response.data.decode())
@@ -114,6 +117,23 @@ class TestUserService(BaseTestCase):
         self.assertEqual(response.status_code, 404)
         self.assertIn('User does not exist', data['message'])
         self.assertIn('fail', data['status'])
+
+    def test_all_users(self):
+        """Ensure get all users behaves correctly."""
+        add_user('test', 'test@test.com')
+        add_user('fletcher', 'fletcher@notreal.com')
+        with self.client:
+            response = self.client.get('/users')
+            data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(data['data']['users']), 2)
+        self.assertIn('test', data['data']['users'][0]['username'])
+        self.assertIn(
+            'test@test.com', data['data']['users'][0]['email'])
+        self.assertIn('fletcher', data['data']['users'][1]['username'])
+        self.assertIn(
+            'fletcher@notreal.com', data['data']['users'][1]['email'])
+        self.assertIn('success', data['status'])
 
 
 
