3dae560b6309ce2e279fcd0f252ffc80e6f9a8d0
Zach Gilbert <zsgilber@gmail.com>
2018-05-26 17:42:32 -0700

    syntax

--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -106,14 +106,14 @@ class TestUserService(BaseTestCase):
         self.assertIn('User does not exist', data['message'])
         self.assertIn('fail', data['status'])
 
-        def test_single_user_incorrect_id(self):
-            """Ensure error is thrown if the id does not exist."""
-            with self.client:
-                response = self.client.get('/users/999')
-                data = json.loads(response.data.decode())
-            self.assertEqual(response.status_code, 404)
-            self.assertIn('User does not exist', data['message'])
-            self.assertIn('fail', data['status'])
+    def test_single_user_incorrect_id(self):
+        """Ensure error is thrown if the id does not exist."""
+        with self.client:
+            response = self.client.get('/users/999')
+            data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 404)
+        self.assertIn('User does not exist', data['message'])
+        self.assertIn('fail', data['status'])
 
 
 
