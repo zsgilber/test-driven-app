e376b93e7f47e948dbbf1ada60b1969b5923905d
Zach Gilbert <zsgilber@gmail.com>
2018-05-26 17:42:48 -0700

    add error handling

--- services/users/project/api/users.py
+++ services/users/project/api/users.py
@@ -47,14 +47,24 @@ def add_user():
 @users_blueprint.route('/users/<user_id>', methods=['GET'])
 def get_single_user(user_id):
     """Get single user details"""
-    user = User.query.filter_by(id=user_id).first()
     response_object = {
-        'status': 'success',
-        'data': {
-            'id': user.id,
-            'username': user.username,
-            'email': user.email,
-            'active': user.active
-        }
+        'status': 'fail',
+        'message': 'User does not exist'
     }
-    return jsonify(response_object), 200
+    try:
+        user = User.query.filter_by(id=user_id).first()
+        if not user:
+            return jsonify(response_object), 404
+        else:
+            response_object = {
+                'status': 'success',
+                'data': {
+                    'id': user.id,
+                    'username': user.username,
+                    'email': user.email,
+                    'active': user.active
+                    }
+                }
+            return jsonify(response_object), 200
+    except ValueError:
+        return jsonify(response_object), 404
