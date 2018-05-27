adab5336a4ccc39276aff6f869e9785e6f3f1a7a
Zach Gilbert <zsgilber@gmail.com>
2018-05-26 17:07:20 -0700

    expanded endpoint to pass tests

--- services/users/project/api/users.py
+++ services/users/project/api/users.py
@@ -20,12 +20,25 @@ def ping_pong():
 @users_blueprint.route('/users', methods=['POST'])
 def add_user():
     post_data = request.get_json()
+    response_object = {
+        'status': 'fail',
+        'message': 'Invalid payload.'
+    }
+    if not post_data:
+        return jsonify(response_object), 400
     username = post_data.get('username')
     email = post_data.get('email')
-    db.session.add(User(username=username, email=email))
-    db.session.commit()
-    response_object = {
-        'status': 'success',
-        'message': f'{email} was added!'
-    }
-    return jsonify(response_object), 201
+    try:
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            db.session.add(User(username=username, email=email))
+            db.session.commit()
+            response_object['status'] = 'success'
+            response_object['message'] = f'{email} was added!'
+            return jsonify(response_object), 201
+        else:
+            response_object['message'] = 'Sorry. That email already exists.'
+            return jsonify(response_object), 400
+    except exc.IntegrityError as e:
+        db.session.rollback()
+        return jsonify(response_object), 400
