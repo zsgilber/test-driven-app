a0f2f74b8fa3fcf108633862446c849bb19846d4
Zach Gilbert <zsgilber@gmail.com>
2018-06-09 20:54:00 -0700

    add comment

--- services/client/src/api/usersApi.js
+++ services/client/src/api/usersApi.js
@@ -5,7 +5,7 @@ class usersApi {
     return axios
       .get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
       .then(response => {
-        const users = response.data.data.users;
+        const users = response.data.data.users; //JSON response has 'data' field, need to get users inside that field
         return users;
       })
       .catch(error => console.log(error));
