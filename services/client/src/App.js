954a65a4c971a724de03d6be28a84739ec6ccd5f
Zach Gilbert <zsgilber@gmail.com>
2018-06-09 17:55:05 -0700

    Flask-debug toolbar and initialize React

---
+++ services/client/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <br/>
+            <h1>All Users</h1>
+            <hr/><br/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
